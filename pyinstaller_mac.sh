99c7fd1886c27c80a1b3a017f64f0463092b17f5
rija1 <rija.ratinahirana@gmail.com>
2022-08-03 11:30:56 +0545

    ok

--- pyinstaller_mac.sh
+++ pyinstaller_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller --specpath bin/mac/spec --distpath bin/mac/dist --workpath bin/mac/build --noconsole --onefile --hidden-import="appscript" audacity_export_mac.py
+pyinstaller --specpath bin/mac/spec --distpath bin/mac/dist --workpath bin/mac/build --noconsole --onefile --hidden-import="appscript" audacity_export.py
